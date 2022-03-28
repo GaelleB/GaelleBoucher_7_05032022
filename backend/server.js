f0ff1154043c4fcd16bdf36d87e4f70056373067
Gaëlle Boucher <gaelleboucher23@gmail.com>
2022-03-28 19:04:38 +0200

    backend et modif frontend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
+// Inportation du package http de Node pour la création d'un serveur Node
 const http = require('http');
+
+// Importation de app.js
 const app = require('./app');
 
+// Configure le port
 app.set('port', process.env.PORT || 3000);
+
+// Création d'un serveur HTTP
 const server = http.createServer(app);
 
+// Ecoute du port (environnement PORT, ou 3000)
 server.listen(process.env.PORT || 3000);
