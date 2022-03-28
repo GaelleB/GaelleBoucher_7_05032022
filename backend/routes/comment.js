ea52de9a2b57a4cc1d04a3a1436ddb90d156c83f
Gaëlle Boucher <gaelleboucher23@gmail.com>
2022-03-28 19:49:31 +0200

    routes et controllers

---
+++ backend/routes/comment.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const commentCtrl = require('../controllers/comment');
+
+router.post('/', commentCtrl.createComment);      
+router.put('/:id', auth, commentCtrl.modifyComment);  
+router.delete('/:id', auth, commentCtrl.deleteComment)
+router.get('/:postId', commentCtrl.getPostComments)  
+router.get('/', auth, commentCtrl.getAllComments)       
+
+module.exports = router;
